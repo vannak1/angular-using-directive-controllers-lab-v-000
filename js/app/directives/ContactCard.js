77e48c82ab5c6847e47ebc43c354ba838a131f06
Vannak Chhay <vannak.chhay1@gmail.com>
2016-08-13 10:53:06 -0400

    Done

--- js/app/directives/ContactCard.js
+++ js/app/directives/ContactCard.js
@@ -19,6 +19,10 @@ function ContactCard() {
 				'<span class="username">{{ username }}</span>',
 			'</div>'
 		].join(''),
+		controller: function($scope) {
+			$scope.username = $scope.username.toLowerCase();
+		},
+		controllerAs: 'ctrl',
 		restrict: 'E'
 	};
 }
